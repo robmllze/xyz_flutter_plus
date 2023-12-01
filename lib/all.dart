6fc6d3bf4ab41879ad6cd19c7f262fd8b24f660d
robmllze <86869208+robmllze@users.noreply.github.com>
2023-12-01 19:48:54 +1100

    update

--- lib/all.dart
+++ lib/all.dart
@@ -1,8 +1,8 @@
 export 'components/all_components.g.dart';
-export 'routing/all_routing.g.dart';
+export 'dart:async';
 export 'package:flutter/material.dart';
+export 'package:meta/meta.dart';
 export 'package:url_launcher/url_launcher.dart';
 export 'package:xyz_gen/all.dart';
+export 'routing/all_routing.g.dart';
 export 'utils/all_utils.g.dart';
-export 'dart:async';
-export 'package:meta/meta.dart';
