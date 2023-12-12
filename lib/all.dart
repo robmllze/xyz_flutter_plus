ab8cf2bafa2101f9535ca4885cc7bc4891d0d12d
robmllze <86869208+robmllze@users.noreply.github.com>
2023-12-12 15:32:10 +1100

    update

--- lib/all.dart
+++ lib/all.dart
@@ -6,7 +6,6 @@ export 'package:url_launcher/url_launcher.dart';
 export 'package:xyz_gen/all.dart';
 export 'package:xyz_utils/html.dart';
 export 'package:xyz_config/all.dart';
-export 'routing/all_routing.g.dart';
 export 'utils/all_utils.g.dart';
 
 export 'package:xyz_pod/xyz_pod.dart';
