010663d460a7637edfec53ba30b2169a3188fbe0
robmllze <86869208+robmllze@users.noreply.github.com>
2024-03-19 14:49:44 +1100

    Simplify

--- CHANGELOG.md
+++
@@ -1,7 +0,0 @@
-# Changelog
-
-## [0.34.0]
-
-- Released @ 2024-03-19 03:46:12.885514Z
-- Bugfix
-- Simplify
--- lib/src/utils/app_properties/app_locale.dart
+++ lib/src/utils/app_properties/app_locale.dart
@@ -28,16 +28,7 @@ class AppLocalePod<T extends AppLocaleEnumMixin> extends AppPropertyPod<T> {
   //
   //
 
-  final TranslationFileReader? fileReader;
-
-  //
-  //
-  //
-
-  AppLocalePod({
-    required super.values,
-    required this.fileReader,
-  }) {
+  AppLocalePod({required super.values}) {
     _pDefault = this;
   }
 
@@ -77,8 +68,7 @@ mixin AppLocaleEnumMixin implements Enum {
   LocaleRef get localeRef => LocaleRef.fromCode(localeCode);
 
   /// The description of this locale.
-  String get localeDescription =>
-      "Locales.descriptions.${this.name}||${this.name}".tr();
+  String get localeDescription => "Locales.descriptions.${this.name}||${this.name}".tr();
 
   /// The translation file reader for this locale.
   TranslationFileReader get translationFileReader {
