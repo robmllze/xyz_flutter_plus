550c7fa05974c4f229b4d96a9c815cde4251cd10
robmllze <86869208+robmllze@users.noreply.github.com>
2024-05-12 21:46:20 +1000

    Added contrast color method

--- lib/src/utils/color/color_utils.dart
+++ lib/src/utils/color/color_utils.dart
@@ -47,6 +47,10 @@ extension ColorUtils on Color {
     final changed = hsl.withLightness(hsl.lightness * (1 - amount));
     return changed.toColor();
   }
+
+  Color contrast(Brightness brighness, double amount) {
+    return brighness == Brightness.light ? this.darken(amount) : this.lighten(amount);
+  }
 }
 
 // ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
@@ -70,8 +74,7 @@ Color mapTextToColor(
   final hue = hashInteger % 360;
 
   // Create a color with the desired saturation, lightness, and alpha values.
-  final baseColor =
-      HSLColor.fromAHSL(1.0, hue.toDouble(), saturation, lightness).toColor();
+  final baseColor = HSLColor.fromAHSL(1.0, hue.toDouble(), saturation, lightness).toColor();
 
   // Blend the base color with the blend color using the specified blend weight.
   final blendedColor = blendWeight > 0.0
