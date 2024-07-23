caabe8dd34c5e4dc5ddb6566b490826fa1ae7991
robmllze <86869208+robmllze@users.noreply.github.com>
2024-07-23 14:43:51 +1000

    Update

--- lib/@screen/src/screen_view.dart
+++ lib/@screen/src/screen_view.dart
@@ -381,6 +381,8 @@ abstract base class ScreenView<
   /// Override to specify the background. This is rendered behind the body.
   Widget background(BuildContext context) {
     return Container(
+      width: double.infinity,
+      height: double.infinity,
       color: Theme.of(context).colorScheme.surface,
     );
   }
