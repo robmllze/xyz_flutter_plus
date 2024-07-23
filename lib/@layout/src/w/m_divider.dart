668aefbea6cc00416e90dfe54e32a3dc8bb8b30e
robmllze <86869208+robmllze@users.noreply.github.com>
2024-07-23 13:11:43 +1000

    Update

--- lib/@layout/src/w/m_divider.dart
+++ lib/@layout/src/w/m_divider.dart
@@ -182,7 +182,7 @@ class MDivider extends StatelessWidget {
 
 // ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
 
-enum WDividerAlignment {
+enum MDividerAlignment {
   //
   //
   //
